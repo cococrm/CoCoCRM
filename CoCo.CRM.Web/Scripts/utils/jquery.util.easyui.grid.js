cffbb90f86999e06a0aec794ad06330ebce5d315
zy <zhengyong710521@163.com>
2015-09-01 18:11:09 +0800

    20150901

--- CoCo.CRM.Web/Scripts/utils/jquery.util.easyui.grid.js
+++ CoCo.CRM.Web/Scripts/utils/jquery.util.easyui.grid.js
@@ -1,13 +1,30 @@
 ﻿(function ($) {
     $.easyui.treegrid = (function () {
+        editIndex = undefined;
         return {
+            //判断编辑
+            endEditing: function (gridId) {
+                if (editIndex == undefined) {
+                    return true;
+                }
+                var grid = $("#" + gridId);
+                if (grid.treegrid('validateRow', editIndex)) {
+                    grid.treegrid('endEdit', editIndex);
+                    editIndex = undefined;
+                    return true;
+                } else {
+                    return false;
+                }
+            },
             //添加并编辑
             addRow: function (gridId) {
-                var grid = $("#" + gridId);
-                var row = { ID: $.newGuid('-'), MenuName: "", MenuCode: "", ParentId: $.newEmptyGuid() };
-                grid.treegrid('append', { parent: '', data: [row] });
-                grid.treegrid('select', row.ID);
-                $.easyui.treegrid.edit(gridId);
+                if ($.easyui.treegrid.endEditing(gridId)) {
+                    var grid = $("#" + gridId);
+                    var row = { ID: $.newGuid('-'), MenuName: "", MenuCode: "", ParentId: $.newEmptyGuid() };
+                    grid.treegrid('append', { parent: '', data: [row], isNewRecord: true });
+                    grid.treegrid('select', row.ID);
+                    $.easyui.treegrid.edit(gridId);
+                }
             },
             //编辑行
             edit: function (gridId) {
@@ -17,16 +34,24 @@
                     $.easyui.warn("请选择待编辑的记录！");
                     return;
                 }
-                grid.treegrid('beginEdit', row.ID);
+                if (editIndex != row.ID) {
+                    if ($.easyui.treegrid.endEditing(gridId)) {
+                        editIndex = row.ID;
+                        grid.treegrid('selectRow', editIndex);
+                        grid.treegrid('beginEdit', editIndex);
+                        alert(row.isNewRecord);
+                    } else {
+                        grid.treegrid('selectRow', editIndex);
+                    }
+                }
             },
             //取消编辑
             cancel: function (gridId) {
+                if (editIndex == undefined) { return; }
                 var grid = $("#" + gridId);
-                var row = grid.treegrid('getSelected');
-                if (!row) {
-                    return;
-                }
-                grid.treegrid('cancelEdit', row.ID);
+                grid.treegrid('cancelEdit', editIndex);
+                //grid.datagrid('rejectChanges');
+                editIndex = undefined;
             },
             //删除节点
             remove: function (gridId) {
